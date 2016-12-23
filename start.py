82e79ed83e86736ecc9fd75c5550ba365fabc023
odran037 <hello@odran037.io>
2016-12-23 12:48:05 -0600

    oh quotes

--- start.py
+++ start.py
@@ -13,7 +13,7 @@ def create_structure(dir):
     for index, keyword in enumerate(keywords):
         directory = '{}/{:02d}-{}'.format(dir, index, keyword)
         filename = '{:02d}-{}.go'.format(index, keyword)
-        path  = "{}/{}".format(directory, filename)
+        path  = '{}/{}'.format(directory, filename)
         os.makedirs(directory)
         if not os.path.exists(path):
             open(path, 'w').close()
@@ -23,4 +23,4 @@ if not os.path.exists(basedir):
     os.makedirs(basedir)
     create_structure(basedir)
 else:
-    print "Directory already exists: {}".format(basedir)
+    print 'Directory already exists: {}'.format(basedir)
